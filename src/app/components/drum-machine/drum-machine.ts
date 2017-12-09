662554d872fc3d9c403b79aa8784ad36ed600690
Tim Cigrand <tcigrand@gmail.com>
2017-12-09 15:47:36 -0700

    Play, pause, and stop support

--- src/app/components/drum-machine/drum-machine.ts
+++ src/app/components/drum-machine/drum-machine.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { clearInterval, setInterval } from 'timers';
 
 @Component({
   selector: 'drum-machine',
@@ -6,5 +7,45 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./drum-machine.scss']
 })
 export class DrumMachine {
-  @Input() track = [];
+  @Input() track: any[] = [];
+  public bpm: number = 120;
+  public currentBeat: number = null;
+  public playing: boolean = false;
+  public paused: boolean = false;
+  private timer: any = null;
+
+  constructor() {}
+
+  public bpmChanged(bpm: number) {
+    this.bpm = bpm;
+    if (this.playing) {
+      this.pause();
+      this.play();
+    }
+  }
+
+  public pause() {
+    this.playing = false;
+    this.paused = true;
+    clearInterval(this.timer);
+  }
+
+  public play() {
+    this.paused = false;
+    this.playing = true;
+    this.timer = setInterval(() => {
+      if (this.currentBeat === null || this.currentBeat === this.track.length - 1) {
+        this.currentBeat = 0;
+      } else {
+        this.currentBeat++;
+      }
+    }, (((60/this.bpm)*4)/this.track.length) * 1000);
+  }
+
+  public stop() {
+    this.playing = false;
+    this.paused = false;
+    clearInterval(this.timer);
+    this.currentBeat = null;
+  }
 }
