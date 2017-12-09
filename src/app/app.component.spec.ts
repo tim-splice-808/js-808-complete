4748d6e4c2531dae2710f9092550d8560216eb0c
Tim Cigrand <tcigrand@gmail.com>
2017-12-09 16:20:02 -0700

    Add tests for app.component and drum-machine

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,24 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
 describe('AppComponent', () => {
+  let fixture;
+  let app;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      imports: [
+        AppModule
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
   }));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+  it(`should define 3 tracks`, async(() => {
+    expect(app.tracks.length).toBe(3);
   }));
 });
---
+++ src/app/components/drum-machine/drum-machine.spec.ts
@@ -0,0 +1,79 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppModule } from '../../app.module';
+import { DrumMachine } from './drum-machine';
+describe('DrumMachine', () => {
+  let fixture;
+  let component;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        AppModule
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DrumMachine);
+    component = fixture.debugElement.componentInstance;
+    component.track = [{
+      kick: false,
+      snare: false,
+      openHat: false,
+      closedHat: false
+    }, {
+      kick: false,
+      snare: false,
+      openHat: false,
+      closedHat: false
+    }, {
+      kick: false,
+      snare: false,
+      openHat: false,
+      closedHat: false
+    }, {
+      kick: false,
+      snare: false,
+      openHat: false,
+      closedHat: false
+    }];
+  }));
+  it('should create the app', async(() => {
+    expect(component).toBeTruthy();
+  }));
+  it(`defaults bpm to 120`, async(() => {
+    expect(component.bpm).toBe(120);
+  }));
+  it(`sets state correctly when playing`, async(() => {
+    component.play();
+    fixture.detectChanges();
+    expect(component.playing).toBe(true);
+    expect(component.paused).toBe(false);
+  }));
+  it(`sets state correctly when paused`, async(() => {
+    component.play();
+    fixture.detectChanges();
+    expect(component.playing).toBe(true);
+    expect(component.paused).toBe(false);
+
+    component.pause();
+    fixture.detectChanges();
+    expect(component.playing).toBe(false);
+    expect(component.paused).toBe(true);
+  }));
+  it(`sets state correctly when stopped`, async(() => {
+    component.play();
+    fixture.detectChanges();
+    expect(component.playing).toBe(true);
+    expect(component.paused).toBe(false);
+
+    component.stop();
+    fixture.detectChanges();
+    expect(component.playing).toBe(false);
+    expect(component.paused).toBe(false);
+    expect(component.currentBeat).toBe(null);
+  }))
+  it(`correctly toggles instrument sound`, async(() => {
+    expect(component.track[0].snare).toBe(false);
+    component.selectAtBeat('snare', 0);
+    expect(component.track[0].snare).toBe(true);
+  }))
+});
