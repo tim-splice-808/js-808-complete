bed28b25ec9343d66f8783e31ec7386da79c2011
Tim Cigrand <tcigrand@gmail.com>
2017-12-10 17:43:41 -0700

    Created new tracks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,251 +10,54 @@ export class AppComponent {
   public tracks = [];
 
   constructor() {
-    const track1 = [{
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }];
+    const track1 = [
+      { kick: true, snare: true, openHat: false, closedHat: true },
+      { kick: false, snare: false, openHat: false, closedHat: true },
+      { kick: false, snare: false, openHat: true, closedHat: true },
+      { kick: false, snare: false, openHat: false, closedHat: true },
+      { kick: true, snare: false, openHat: false, closedHat: true },
+      { kick: false, snare: false, openHat: false, closedHat: true },
+      { kick: false, snare: false, openHat: false, closedHat: false },
+      { kick: false, snare: false, openHat: false, closedHat: true },
+      { kick: true, snare: true, openHat: false, closedHat: true },
+      { kick: false, snare: false, openHat: false, closedHat: true },
+      { kick: false, snare: false, openHat: false, closedHat: false },
+      { kick: false, snare: false, openHat: false, closedHat: true },
+      { kick: true, snare: true, openHat: false, closedHat: true },
+      { kick: false, snare: false, openHat: false, closedHat: true },
+      { kick: false, snare: false, openHat: false, closedHat: true },
+      { kick: true, snare: false, openHat: false, closedHat: true }
+    ];
 
-    const track2 = [{
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: true,
-      openHat: true,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }];
+    const track2 = [
+      { kick: true, snare: false, openHat: false, closedHat: false },
+      { kick: false, snare: false, openHat: false, closedHat: true },
+      { kick: false, snare: true, openHat: false, closedHat: true },
+      { kick: true, snare: false, openHat: false, closedHat: false },
+      { kick: true, snare: false, openHat: false, closedHat: false },
+      { kick: false, snare: false, openHat: false, closedHat: true },
+      { kick: false, snare: true, openHat: false, closedHat: true },
+      { kick: true, snare: false, openHat: true, closedHat: false }
+    ];
 
-    const track3 = [{
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: false,
-      openHat: false,
-      closedHat: true
-    }, {
-      kick: true,
-      snare: true,
-      openHat: true,
-      closedHat: true
-    }];
+    const track3 = [
+      { kick: true, snare: false, openHat: false, closedHat: false },
+      { kick: true, snare: false, openHat: false, closedHat: false },
+      { kick: false, snare: true, openHat: false, closedHat: false },
+      { kick: true, snare: false, openHat: true, closedHat: false },
+      { kick: true, snare: false, openHat: false, closedHat: true },
+      { kick: false, snare: false, openHat: false, closedHat: true },
+      { kick: false, snare: false, openHat: true, closedHat: false },
+      { kick: true, snare: true, openHat: false, closedHat: false },
+      { kick: true, snare: true, openHat: false, closedHat: false },
+      { kick: false, snare: true, openHat: false, closedHat: false },
+      { kick: false, snare: false, openHat: false, closedHat: true },
+      { kick: true, snare: false, openHat: false, closedHat: true },
+      { kick: true, snare: false, openHat: false, closedHat: false },
+      { kick: false, snare: true, openHat: false, closedHat: false },
+      { kick: true, snare: false, openHat: true, closedHat: false },
+      { kick: true, snare: false, openHat: false, closedHat: false }
+    ];
 
     this.tracks = [track1, track2, track3];
   }
